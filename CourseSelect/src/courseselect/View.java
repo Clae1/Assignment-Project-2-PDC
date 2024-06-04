a95deeb81fb45d6d593309714b76180ee4c3460f
Clae <claeone1@gmail.com>
2024-06-05 01:40:20 +1200

    Commiting

---
+++ CourseSelect/src/courseselect/View.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package courseselect;
+
+import java.awt.event.ActionListener;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author claeo
+ */
+public class View extends JFrame implements Observer
+{
+    
+    
+    
+    //Constructor for components of the JFrame Panel
+    public View()
+    {
+        
+    }
+
+    @Override
+    public void update(Observable o, Object arg) 
+    {
+        
+    }
+    
+    
+    //Inside this method will contain all the buttons for each
+    //page of the GUI
+    public void addActionListener(ActionListener listener)
+    {
+        
+    }
+    
+}
